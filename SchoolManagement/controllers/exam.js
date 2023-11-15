0ccffcb1193f14ea86e4e2e8c6a1eec132a8b839
Akshaya.kcoddle <akshaya.kcoddletech.com>
2023-11-15 14:34:12 +0530

    exam api

--- SchoolManagement/controllers/exam.js
+++ SchoolManagement/controllers/exam.js
@@ -1,25 +1,20 @@
 const examModel = require('../models/exam');
 
 const sheduleExam = async (req, res) => {
-    let result;
-    let result2;
     try {
         const { name, classId, startDate, endDate, subjectId, roomNumber, academicYear, examTypeId } = req.body;
         if (!name || !classId || !startDate || !endDate || !subjectId || !roomNumber || !academicYear) {
             return res.status(400).send({ message: 'missing required field', success: false });
         }
-        result2 = await examModel.sheduleinsert(name, classId, startDate, endDate, subjectId, roomNumber, academicYear, examTypeId)
-        if (result2.length > 0) {
-            res.status(401).send({ message: 'already exist', success: true });
-        } else {
-            // result = examModel.shedule(name, classId, startDate, endDate, subjectId, roomNumber, academicYear, examTypeId)
-            res.status(200).send({ message: 'data added successfully', success: true });
 
+        const result2 = await examModel.sheduleinsert(name, classId, startDate, endDate, subjectId, roomNumber, academicYear, examTypeId);
+
+        if (result2 && result2.length > 0) {
+            res.status(401).send({ message: 'Conflict detected', success: true, conflicts: result2 });
         }
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).send({ error: 'failed to insert into database', success: false });
+    } catch (err) {
+        console.error("Error in sheduleExam:", err);
+        res.status(500).send({ error: 'Failed to insert into database', success: false });
     }
 };
 
--- SchoolManagement/models/exam.js
+++ SchoolManagement/models/exam.js
@@ -6,43 +6,42 @@ const sheduleinsert = async (name, classId, startDate, endDate, subjectId, roomN
     try {
         const roomAvilability = `SELECT * FROM exam WHERE room_number ='${roomNumber}'`;
         const checkClass = `SELECT * FROM exam WHERE class_id = ${classId}`;
+
         const resClass = await db.query(checkClass);
         const resRoom = await db.query(roomAvilability);
 
-        if (resClass.length > 0) {
-            for (const examEntry of resClass) {
-                const timeduration = `SELECT * FROM exam WHERE (start_date BETWEEN '${startDate}' AND '${endDate}' OR end_date BETWEEN '${startDate}' AND '${endDate}') AND id = ${examEntry.id}`;
-                const resTimeDuration = await db.query(timeduration);
+        let hasClassConflict = false;
+        let hasRoomConflict = false;
 
-                if (resTimeDuration.length > 0) {
-                    console.log("Conflict with class schedule");
-                    return resTimeDuration;
-                }
-                else {
-                    // No conflicts, proceed with insertion
-                    const qr = `INSERT INTO exam (name, class_id, start_date, end_date, subject_id, room_number, academic_year, exam_type_id) VALUES ('${name}', ${classId}, '${startDate}', '${endDate}', ${subjectId}, '${roomNumber}', ${academicYear}, ${examTypeId});`;
-                    result = await db.query(qr);
-                    console.log("No Conflict with class  Data inserted successfully");
-                    return result;
-                }
+        for (const examEntry of resClass) {
+            const timeduration = `SELECT * FROM exam WHERE (start_date BETWEEN '${startDate}' AND '${endDate}' OR end_date BETWEEN '${startDate}' AND '${endDate}') AND id = ${examEntry.id}`;
+            const resTimeDuration = await db.query(timeduration);
 
+            if (resTimeDuration.length > 0) {
+                console.log("Conflict with class schedule");
+                hasClassConflict = true;
+                break; // exit the loop if there is a conflict
             }
-        } else if (resRoom.length > 0) {
-            console.log("hi");
+        }
+
+        if (!hasClassConflict) {
             for (const examEntry of resRoom) {
                 const durationRoom = `SELECT * FROM exam WHERE (start_date BETWEEN '${startDate}' AND '${endDate}' OR end_date BETWEEN '${startDate}' AND '${endDate}') AND id = ${examEntry.id}`;
                 const resTimeRoom = await db.query(durationRoom);
 
                 if (resTimeRoom.length > 0) {
                     console.log("Conflict with room schedule");
-                    return resTimeRoom;
+                    hasRoomConflict = true;
+                    break; // exit the loop if there is a conflict
                 }
             }
-        } else {
+        }
+
+        if (!hasClassConflict && !hasRoomConflict) {
             // No conflicts, proceed with insertion
             const qr = `INSERT INTO exam (name, class_id, start_date, end_date, subject_id, room_number, academic_year, exam_type_id) VALUES ('${name}', ${classId}, '${startDate}', '${endDate}', ${subjectId}, '${roomNumber}', ${academicYear}, ${examTypeId});`;
             result = await db.query(qr);
-            console.log(" no Conflict with room Data inserted successfully");
+            console.log("Data inserted successfully");
             return result;
         }
     } catch (error) {
@@ -53,7 +52,6 @@ const sheduleinsert = async (name, classId, startDate, endDate, subjectId, roomN
     }
 };
 
-
 module.exports = {
     sheduleinsert
-}
+};
