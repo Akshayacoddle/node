fb2494543df822fcd00b4ddbc9616e9eb7759862
Akshaya.kcoddle <akshaya.kcoddletech.com>
2023-12-01 11:05:23 +0530

    studentreview api

--- SchoolManagement/routes/student.js
+++ SchoolManagement/routes/student.js
@@ -11,6 +11,6 @@ router.use(jsonParser);
 router.post('/register', signController.createData);
 router.post('/login', signController.login);
 router.get('/view', jwt.verifyJwt, signController.viewStudent);
-router.post('/review', signController.studentReview);
+router.post('/feedback', signController.studentReview);
 
 module.exports = router;
